be88a86d1de7575851bd60455ac43c8bf57578c2
Yoha30 <3202114888@alexu.edu.eg>
2021-09-27 23:35:26 +0200

    bootstrap

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-setInterval(function (){
-var countdate = new Date("27 Sep , 2021 , 22:00:00").getTime();
+ var x = setInterval(function (){
+var countdate = new Date("29 Sep , 2021 , 22:00:00").getTime();
 var today = new Date().getTime();
-var timeLeft = countdate - today;
+var timeLeft = (countdate - today);
 var days = Math.floor(timeLeft/(1000*60*60*24));
 var hours = Math.floor(timeLeft % (1000*60*60*24)/(1000*60*60));
 var mins = Math.floor(timeLeft % (1000*60*60)/(1000*60));
@@ -10,4 +10,5 @@ document.getElementById('date').innerText = "0" + days;
 document.getElementById('hours').innerText = hours;
 document.getElementById('mins').innerText = mins;
 document.getElementById('sec').innerText = sec;
+
 },1000);
